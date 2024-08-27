cfb23760c00f9798de3fdc05ad6582073497aa03
elanovviktor <elanov_viktor@mail.ru>
2024-08-27 14:56:55 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1st program
+print(9**0.5*5)
+# 2 st program
+print(9.99>9.98,1000!=1001)
+# 3st program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+# 4st program
+numbers = '123.456'
+numbers = 123.456
+print(numbers)
+
+print(int(123.456*10))
+print(int(1234 % 10))
+
+
